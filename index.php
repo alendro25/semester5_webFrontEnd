da282b1cb35ab3fbb0934e51035b5bb7e16b9a53
alendro25 <57282213+alendro25@users.noreply.github.com>
2021-01-10 18:23:23 +0700

    Komit Pertama

---
+++ index.php
@@ -0,0 +1,246 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Google Fonts 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    -->
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+
+    <title>Web Sederhana</title>
+</head>
+  
+  <body>
+    
+    <!--NavBar-->
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar fixed-top">
+        <div class="container">
+          <a class="navbar-brand" href="#">Gerald</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                More
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Download</a>
+                <a class="dropdown-item" href="#">FAQ</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">About</a>
+              </div>
+            </li>
+            
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+          </div>
+        </div>
+    </nav>
+
+    <!--Header(JumboTron)-->
+    <header>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container text-center">
+              <img src="img/Header.jpeg" width="25%" class="rounded-circle"> 
+                        <h1>Welcome to My Web</h1>
+                        <p class="lead"><t>Unitomo | Teknik Informatika</t></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    <br>
+    <br>
+  </header>
+   <!-- About-->
+   <div class="container">
+    <div class="row">
+      <div class="col text-center">
+        <h1>About</h1>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col text-right"><p>Nama :<br>NIM :<br>Kelas :</p></div>
+      <div class="col"><p>Gerald Alessandro Timothy<br>2018420126<br>K(malam)</p></div>
+    </div>
+  </div>
+    <br>
+    <br>
+
+    <!-- Content-->
+    <div class="container">
+      <div class="row">
+        <div class="col text-center">
+          <h1>Material</h1>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md mx-auto">
+          <div class="card" >
+            <img src="img/html.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Belajar HTML</h5>
+              <p class="card-text">HTML adalah singkatan dari Hypertext Markup Language. HTML memungkinkan seorang user untuk membuat dan menyusun bagian paragraf, heading, link atau tautan, dan blockquote untuk halaman web dan aplikasi.</p>
+              <a href="#" class="btn btn-primary">Show more</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md">
+          <div class="card" >
+            <img src="img/php.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Belajar PHP</h5>
+              <p class="card-text">Pengertian PHP, PHP Adalah bahasa scripting server-side, Bahasa pemrograman yang digunakan untuk mengembangkan situs web statis atau situs web dinamis atau aplikasi Web. PHP singkatan dari Hypertext Pre-processor, yang sebelumnya disebut Personal Home Pages.</p>
+              <a href="#" class="btn btn-primary">Show more</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md">
+          <div class="card" >
+            <img src="img/css.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Belajar CSS</h5>
+              <p class="card-text">Cascading Style Sheet (CSS) merupakan aturan untuk mengatur beberapa komponen dalam sebuah web sehingga akan lebih terstruktur dan seragam. CSS bukan merupakan bahasa pemograman.</p>
+              <a href="#" class="btn btn-primary">Show more</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md">
+          <div class="card" >
+            <img src="img/js.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Belajar JavaScript</h5>
+              <p class="card-text">Bagi yang masih awam, tentu akan bertanya-tanya, apa itu JavaScript? JavaScript adalah salah satu bahasa pemrograman yang paling banyak digunakan dalam kurun waktu dua puluh tahun ini.</p>
+              <a href="#" class="btn btn-primary">Show more</a>
+            </div>
+          </div>
+        </div>
+
+        
+
+      </div>
+    </div>
+    <br>
+
+    <!-- Footer-->
+    <footer class="page-footer bg-dark">
+
+      <div class="bg-success">
+        <div class="container">
+          <div class="row py-4 d-flex align-item-center">
+
+            <div class="col-md-12 text-center">
+              <a href=""><i class="fab fa-facebook-square ml-3"></i> </a>
+              <a href=""><i class="fab fa-twitter ml-3"></i> </a>
+              <a href=""><i class="fab fa-google ml-3"></i> </a>
+              <a href=""><i class="fab fa-instagram ml-3"></i> </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container text-center text-md-left mt-5">
+        <div class="row">
+
+          <div class="col-md-3 mx-auto mb-4">
+            <h6 class="text-uppercase font-weigh-bold">Gerald</h6>
+            <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; height: 2px;">
+            <p class="mt-2">Mahasiswa unitomo prodi informatika<br>dengan nim 2018420126</p>
+          </div>
+
+          <div class="col-md-3 mx-auto mb-4">
+            <h6 class="text-uppercase font-weigh-bold">Material</h6>
+            <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto" style="width: 75px; height: 2px;">
+            
+            <ul class="list-unstyled">
+              <li class="my-2"><a href="#">HTML 5</a></li>
+              <li class="my-2"><a href="#">PHP</a></li>
+              <li class="my-2"><a href="#">CSS</a></li>
+              <li class="my-2"><a href="#">JavaScript</a></li>
+            </ul>
+          </div>
+
+          <div class="col-md-2 mx-auto mb-4">
+            <h6 class="text-uppercase font-weigh-bold">Usefull Link</h6>
+            <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto" style="width: 110px; height: 2px;">
+
+            <ul class="list-unstyled">
+              <li class="my-2"><a href="#">Home</a></li>
+              <li class="my-2"><a href="#">About</a></li>
+              <li class="my-2"><a href="#">Services</a></li>
+              <li class="my-2"><a href="#">Contact</a></li>
+            </ul>
+          </div>
+
+          <div class="col-md-3 mx-auto mb-4">
+            <h6 class="text-uppercase font-weigh-bold">Contact</h6>
+            <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto" style="width: 110px; height: 2px;">
+
+            <ul class="list-unstyled">
+              <li class="my-2"><i class="fas fa-home"></i>   Griya Kebraon Timur FA/30</li>
+              <li class="my-2"><i class="fas fa-envelope"></i>   alendro25@gmail.com</li>
+              <li class="my-2"><i class="fas fa-phone"></i>   0838-3114-4432</li>
+              <li class="my-2"><i class="fas fa-print"></i>   (031)7667262</li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="footer-copyright text-center py-3">
+        <p>&copy;Copyright
+          <a href="#">gerald.com</a>
+        </p>
+        <p>Designed by Me</p>
+      </div>
+
+    </footer>
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>    
